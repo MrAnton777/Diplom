0c534e57eda7caeabccc6d6f529ed1bb2d7f7af5
MrAnton777 <anton20ivanoff@yandex.ru>
2025-04-23 18:59:36 +0300

    2.1.6 done

--- src/hotels/hotels.controller.ts
+++ src/hotels/hotels.controller.ts
@@ -3,16 +3,23 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Hotel,HotelDocument } from './schemas/hotel.schema';
 import { Room,RoomDocument } from './schemas/room.schema';
-import { Get,Post,Body,Query,Param,Res } from '@nestjs/common';
-import { SearchRoomsParams } from './interfaces/interfaces';
+import { Get,Post,Body,Query,Put,Param,Res } from '@nestjs/common';
+import { SearchHotelParams, SearchRoomsParams, UpdateHotelParams } from './interfaces/interfaces';
 import { CurrentUser } from 'src/auth/dto/user.decorator';
 import { User } from 'src/users/schemas/users.schema';
 import { RoomService,HotelService } from './hotels.service';
 import { ID } from 'src/types/types';
-import { createHotelDto } from './interfaces/dto';
+import { createHotelDto, createRoomDto } from './interfaces/dto';
 import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
-import { error } from 'console';
+import { error, time } from 'console';
 import { Response } from 'express';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { UseInterceptors } from '@nestjs/common';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path'
+import { UploadedFiles } from '@nestjs/common';
+import { Express } from 'express';
 
 
 @Controller('api')
@@ -44,6 +51,57 @@ export class HotelsController {
         data = {...data,createdAt:new Date()}
 
         return await this.hotelService.create(data)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('admin/hotels/') //2.1.4
+    async getHotels(@Query() params:SearchHotelParams,@CurrentUser() user:User,@Res() res:Response){
+        if (user.role != 'admin') return res.status(403).json({error:403,message:'У вас нет прав'});
+
+        return await this.hotelService.search(params)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Put('admin/hotels/:id') //2.1.5
+    async updateHotel(@Param() id:ID,@Body() data:UpdateHotelParams,@Res() res:Response,@CurrentUser() user:User){
+        if (user.role != 'admin') return res.status(403).json({error:403,message:'У вас нет прав'});
+
+        let hotel =  await this.hotelService.update(id,data)
+
+        return {
+            id:hotel._id,
+            title:hotel.title,
+            desc:hotel.desc
+        }
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('admin/hotel-rooms/') //2.1.6
+    @UseInterceptors(FilesInterceptor('images', 10, {
+        storage: diskStorage({
+          destination: './uploads',
+          filename: (req, file, cb) => {
+            const filename: string = uuidv4();
+            const extension: string = path.parse(file.originalname).ext;
+            cb(null, `${filename}${extension}`);
+          }
+        })
+      }))
+    async createRoom(@Body() dataDto:createRoomDto,@Res() res:Response,@CurrentUser() user:User,@UploadedFiles() images:Express.Multer.File[]){
+        if (user.role != 'admin') return res.status(403).json({error:403,message:'У вас нет прав'});
+
+        let imagesPaths = images.map(image => image.filename)
+
+        let data = {
+            hotel:dataDto.hotelId,
+            desc:dataDto.description,
+            images:imagesPaths,
+            createdAt:new Date(),
+            updatedAt:new Date(),
+            isEnabled:true
+        } as Room
+
+        return await this.roomService.create(data)
     }
 
 } 
--- src/hotels/interfaces/dto.ts
+++ src/hotels/interfaces/dto.ts
@@ -1,5 +1,12 @@
+import { Types } from "mongoose";
+
 export interface createHotelDto{
     title: string;
     description: string;
     createdAt?:Date
+}
+
+export interface createRoomDto{
+    description:string,
+    hotelId:Types.ObjectId
 }
